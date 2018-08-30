152f468000b4d2b0484e87903c904874f0204a60
Peng Yu <PYu3@schools.nyc.gov>
2018-08-30 09:39:55 -0400

    c

--- GeoX/GeoSupport/App_Code/GeoSupportService.cs
+++ GeoX/GeoSupport/App_Code/GeoSupportService.cs
@@ -14,15 +14,10 @@ using GeoSupportService;
 [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1, Name = "GeoSupportServiceSoap", Namespace = "http://schools.nyc.gov/DOE.Services.Geography/GeoSupportService_v1_0")]
 public partial class GeoSupportServiceSoap : System.Web.Services.WebService, IGeoSupportServiceSoap
 {
+    private static log4net.ILog Log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
 
     public GeoSupportServiceSoap()
     {
-    }
-
-    [WebMethod]
-    public string HelloWorld()
-    {
-        return "Hello World, Peng";
     }
 
     [System.Web.Services.WebMethodAttribute()]
@@ -34,23 +29,57 @@ public partial class GeoSupportServiceSoap : System.Web.Services.WebService, IGe
 
     }
 
-    public PropertyLevelInformationByAddressResults PropertyLevelInformationbyAddress(string HouseNumber, string StreetName, string Borough)
+    [System.Web.Services.WebMethodAttribute()]
+    [System.Web.Services.Protocols.SoapDocumentMethodAttribute("PropertyLevelInformationByAddressResults", RequestNamespace = "http://schools.nyc.gov/DOE.Services.Geography/GeoSupportService_v1_0", ResponseNamespace = "http://schools.nyc.gov/DOE.Services.Geography/GeoSupportService_v1_0", Use = System.Web.Services.Description.SoapBindingUse.Literal, ParameterStyle = System.Web.Services.Protocols.SoapParameterStyle.Wrapped, Binding = "GeoSupportServiceSoap")]
+    [return: System.Xml.Serialization.XmlElementAttribute(Form = System.Xml.Schema.XmlSchemaForm.Unqualified)]
+    public virtual PropertyLevelInformationByAddressResults PropertyLevelInformationbyAddress([System.Xml.Serialization.XmlElementAttribute(Form = System.Xml.Schema.XmlSchemaForm.Unqualified, ElementName = "HouseNumber")] string HouseNumber, [System.Xml.Serialization.XmlElementAttribute(Form = System.Xml.Schema.XmlSchemaForm.Unqualified, ElementName = "StreetName")] string StreetName, [System.Xml.Serialization.XmlElementAttribute(Form = System.Xml.Schema.XmlSchemaForm.Unqualified, ElementName = "Borough")] string Borough)
     {
-        throw new NotImplementedException();
+        return ServiceLogic.PropertyLevelInformationbyAddress(HouseNumber, StreetName, Borough);
+
     }
 
-    public GeographicInformationByIntersectionResults GeographicInformationbyIntersection(string StreetName1, string Borough1, string StreetName2, string Borough2)
+    [System.Web.Services.WebMethodAttribute()]
+    [System.Web.Services.Protocols.SoapDocumentMethodAttribute("GeographicInformationbyIntersection", RequestNamespace = "http://schools.nyc.gov/DOE.Services.Geography/GeoSupportService_v1_0", ResponseNamespace = "http://schools.nyc.gov/DOE.Services.Geography/GeoSupportService_v1_0", Use = System.Web.Services.Description.SoapBindingUse.Literal, ParameterStyle = System.Web.Services.Protocols.SoapParameterStyle.Wrapped, Binding = "GeoSupportServiceSoap")]
+    [return: System.Xml.Serialization.XmlElementAttribute(Form = System.Xml.Schema.XmlSchemaForm.Unqualified)]
+    public virtual GeographicInformationByIntersectionResults GeographicInformationbyIntersection([System.Xml.Serialization.XmlElementAttribute(Form = System.Xml.Schema.XmlSchemaForm.Unqualified, ElementName = "StreetName1")] string StreetName1, [System.Xml.Serialization.XmlElementAttribute(Form = System.Xml.Schema.XmlSchemaForm.Unqualified, ElementName = "Borough1")] string Borough1, [System.Xml.Serialization.XmlElementAttribute(Form = System.Xml.Schema.XmlSchemaForm.Unqualified, ElementName = "StreetName2")] string StreetName2, [System.Xml.Serialization.XmlElementAttribute(Form = System.Xml.Schema.XmlSchemaForm.Unqualified, ElementName = "Borough2")] string Borough2)
     {
-        throw new NotImplementedException();
+        return ServiceLogic.GeographicInformationbyIntersection(StreetName1, Borough1, StreetName2, Borough2);
+
     }
 
-    public ZoneInfoByAddressResultsZoneInfoByAddress[] ZoneInformationByAddress(AddressInfo zoneInformationByAddress1)
+    [System.Web.Services.WebMethodAttribute()]
+    [System.Web.Services.Protocols.SoapDocumentMethodAttribute("urn:ZoneInformationByAddress", Use = System.Web.Services.Description.SoapBindingUse.Literal, ParameterStyle = System.Web.Services.Protocols.SoapParameterStyle.Bare, Binding = "GeoSupportServiceSoap")]
+    [return: System.Xml.Serialization.XmlArrayAttribute("ZoneInformationByAddressResponse", Namespace = "http://schools.nyc.gov/DOE.Services.Geography/GeoSupportService_v1_0")]
+    [return: System.Xml.Serialization.XmlArrayItemAttribute("ZoneInfoByAddress", IsNullable = false)]
+    public virtual ZoneInfoByAddressResultsZoneInfoByAddress[] ZoneInformationByAddress([System.Xml.Serialization.XmlElementAttribute("ZoneInformationByAddress", Namespace = "http://schools.nyc.gov/DOE.Services.Geography/GeoSupportService_v1_0")] AddressInfo zoneInformationByAddress1)
     {
-        throw new NotImplementedException();
+        Log.Debug(string.Format("ZoneInformationByAddress params: {0} --- {1} --- {2}", zoneInformationByAddress1.HouseNumber, zoneInformationByAddress1.StreetName, zoneInformationByAddress1.BoroughOrZipCode));
+        ZoneInfoByAddressResultsZoneInfoByAddress[] results = ServiceLogic.ZoneInfobyAddressResultsZoneInfoByAddress(zoneInformationByAddress1.HouseNumber, zoneInformationByAddress1.StreetName, zoneInformationByAddress1.BoroughOrZipCode);
+        Log.Debug("Service Logic Successful");
+        return results;
+
     }
 
-    public EnrollmentInformationByAddressResultsEnrollmentInfoByAddress[] EnrollmentInformationByAddress(AddressInfo enrollmentInformationByAddress1)
+    [System.Web.Services.WebMethodAttribute()]
+    [System.Web.Services.Protocols.SoapDocumentMethodAttribute("urn:EnrollmentInformationByAddress", Use = System.Web.Services.Description.SoapBindingUse.Literal, ParameterStyle = System.Web.Services.Protocols.SoapParameterStyle.Bare, Binding = "GeoSupportServiceSoap")]
+    [return: System.Xml.Serialization.XmlArrayAttribute("EnrollmentInformproxyationByAddressResponse", Namespace = "http://schools.nyc.gov/DOE.Services.Geography/GeoSupportService_v1_0")]
+    [return: System.Xml.Serialization.XmlArrayItemAttribute("EnrollmentInfoByAddress", IsNullable = false)]
+    public virtual EnrollmentInformationByAddressResultsEnrollmentInfoByAddress[] EnrollmentInformationByAddress([System.Xml.Serialization.XmlElementAttribute("EnrollmentInformationByAddress", Namespace = "http://schools.nyc.gov/DOE.Services.Geography/GeoSupportService_v1_0")] AddressInfo enrollmentInformationByAddress1)
     {
-        throw new NotImplementedException();
+        Log.Debug(string.Format("EnrollmentInformationByAddress params: {0} --- {1} --- {2}", enrollmentInformationByAddress1.HouseNumber, enrollmentInformationByAddress1.StreetName, enrollmentInformationByAddress1.BoroughOrZipCode));
+        EnrollmentInformationByAddressResultsEnrollmentInfoByAddress[] results = ServiceLogic.EnrollmentInformationByAddressResultsEnrollmentInfoByAddress(enrollmentInformationByAddress1.HouseNumber, enrollmentInformationByAddress1.StreetName, enrollmentInformationByAddress1.BoroughOrZipCode);
+        return results;
     }
+
+    [System.Web.Services.WebMethodAttribute()]
+    [System.Web.Services.Protocols.SoapDocumentMethodAttribute("urn:NYCInformationByAddress", Use = System.Web.Services.Description.SoapBindingUse.Literal, ParameterStyle = System.Web.Services.Protocols.SoapParameterStyle.Bare, Binding = "GeoSupportServiceSoap")]
+    [return: System.Xml.Serialization.XmlArrayAttribute("NYCInformationByAddressResponse", Namespace = "http://schools.nyc.gov/DOE.Services.Geography/GeoSupportService_v1_0")]
+    [return: System.Xml.Serialization.XmlArrayItemAttribute("NYCInformationByAddress", IsNullable = false)]
+    public virtual NYCInformationByAddressResultsNYCInformationByAddress[] NYCInformationByAddress([System.Xml.Serialization.XmlElementAttribute("NYCInformationByAddress", Namespace = "http://schools.nyc.gov/DOE.Services.Geography/GeoSupportService_v1_0")] AddressInfo nYCInformationByAddress1)
+    {
+        Log.Debug(string.Format("NYCInformationByAddress params: {0} --- {1} --- {2}", nYCInformationByAddress1.HouseNumber, nYCInformationByAddress1.StreetName, nYCInformationByAddress1.BoroughOrZipCode));
+        NYCInformationByAddressResultsNYCInformationByAddress[] results = ServiceLogic.NYCInformationByAddressResultsNYCInformationByAddress(nYCInformationByAddress1.HouseNumber, nYCInformationByAddress1.StreetName, nYCInformationByAddress1.BoroughOrZipCode);
+        return results;
+    }
+
 }
